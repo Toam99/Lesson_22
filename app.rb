d6e79ad613adbafa0e088fda7e50b773d9679cbe
Toam99 <60461717+Toam99@users.noreply.github.com>
2020-06-15 20:12:18 +0800

    Add about page

--- app.rb
+++ app.rb
@@ -4,8 +4,27 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School !!!</a>"			
+	   erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School !!!</a>"			
 end
 
+get '/about' do      
+    erb :about
+end
+
+get '/visit' do
+    "Register"
+end
+
+get '/contacts' do
+    "Contacts"
+end
+
+
+
+
 # 1 change "My Website" into "barbe Shop"
-# 2
+# 2 add pages --> /about, /visit, /contacts,
+# 3
+# 4
+# 5
+# 6 
