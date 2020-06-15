b1ae1f68d0e4b671afb70da88abd72fc068d8939
Toam99 <60461717+Toam99@users.noreply.github.com>
2020-06-15 19:25:29 +0800

    Update title

--- app.rb
+++ app.rb
@@ -6,3 +6,6 @@ require 'sinatra/reloader'
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School !!!</a>"			
 end
+
+# 1 change "My Website" into "barbe Shop"
+# 2
